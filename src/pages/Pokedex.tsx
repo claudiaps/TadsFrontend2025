7e609c5a909cce1522e83868095d86db7e5f4194
claudiaps <clp.sampedro@gmail.com>
2025-03-20 19:36:02 -0300

    inicio pokedex

--- src/pages/Pokedex.tsx
+++ src/pages/Pokedex.tsx
@@ -1,13 +1,32 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Pokedex = () => {
   // Variável de estado para armazenar o valor do input
   const [inputText, setInputText] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [pokemon, setPokemon] = useState();
 
   //   Função que irá buscar o Pokémon
-  const searchPokemon = () => {
-    console.log(inputText);
+  const searchPokemon = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${inputText}`
+      );
+      setPokemon(data);
+      console.log({ data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
   };
+  console.log(pokemon);
+
+  if (loading) {
+    return <div>Carregando...</div>;
+  }
 
   return (
     <div>
@@ -19,6 +38,9 @@ const Pokedex = () => {
       />
       {/* Botão para disparar a busca do pokémon */}
       <button onClick={searchPokemon}>Buscar</button>
+      <p>Nome: {pokemon?.name}</p>
+      <p>Peso: {pokemon?.weight}</p>
+      <img src={pokemon?.sprites?.front_default} />
     </div>
   );
 };
