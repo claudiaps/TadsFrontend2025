bea17c3d1af2b14940c32b15c389da0f86024ffa
claudiaps <clp.sampedro@gmail.com>
2025-03-20 21:49:53 -0300

    adicionando requisição para pegar o pokemon

--- src/pages/PokemonInfo.tsx
+++ src/pages/PokemonInfo.tsx
@@ -1,18 +1,49 @@
-import { useCallback, useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
+type Pokemon = {
+  name: string;
+  sprites: {
+    front_default: string;
+    front_shiny: string;
+  };
+  weight: number;
+  types: { type: { name: string } }[];
+};
+
 const PokemonInfo = () => {
+  const [loading, setLoading] = useState(false);
+  const [pokemon, setPokemon] = useState<Pokemon>();
   const { name } = useParams();
 
-  const getPokemonInfo = useCallback(async () => {}, []);
+  const getPokemonInfo = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.get<Pokemon>(
+        `https://pokeapi.co/api/v2/pokemon/${name}`
+      );
+      setPokemon(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     getPokemonInfo();
   }, []);
 
+  if (loading) {
+    return <div>Carregando Pokémon</div>;
+  }
+
   return (
     <div>
       <h1>Pokemon Info - {name}</h1>
+      <p>Nome: {pokemon?.name}</p>
+      <p>Peso: {pokemon?.weight}kg</p>
     </div>
   );
 };
